e54d999f2c714ad74e727b955ad79b26d2dca530
sjq <353230345@qq.com>
2018-08-03 11:09:55 +0800

    res

--- main/main.js
+++ main/main.js
@@ -3,12 +3,25 @@ module.exports = function main(data) {
 	3:'._.'+'\n'+'._|'+'\n'+'._|',4:'...'+'\n'+'|_|'+'\n'+'..|',5:'._.'+'\n'+'|_.'+'\n'+'._|',
 	6:'._.'+'\n'+'|_.'+'\n'+'|_|',7:'._.'+'\n'+'..|'+'\n'+'..|',8:'._.'+'\n'+'|_|'+'\n'+'|_|',
 	9:'._.'+'\n'+'|_|'+'\n'+'..|'};
-    var a=data/100;
-	var b=(data/10)%10;
-	var c=data%10;
-    conlog.(obj.a);
-    conlog.(obj.b);
-    conlog.(obj.c);
+	if(data>=0&&data<10){
+		var a=data%10;
+		conlog.(obj.a);
+	}else if(data<100){
+		var a=(data/10)%10;
+		var b=data%10;
+		conlog.(obj.a);
+		conlog.(obj.b);
+	}else if(data<1000){
+		var a=data/100;
+	    var b=(data/10)%10;
+	    var c=data%10;
+        conlog.(obj.a);
+        conlog.(obj.b);
+        conlog.(obj.c);
+	}else{
+		break;
+	}
+  
 };
 
 
