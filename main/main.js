6db70a99e037e7163598438551a360c9ee8711f9
sjq <353230345@qq.com>
2018-08-03 11:04:06 +0800

    res

--- main/main.js
+++ main/main.js
@@ -1,4 +1,14 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(data) {
+	var obj={0:'._.'+'\n'+'|.|'+'\n'+'|_|',1:'...'+'\n'+'..|'+'\n'+'..|',2:'._.'+'\n'+'._|'+'\n'+'|_.',
+	3:'._.'+'\n'+'._|'+'\n'+'._|',4:'...'+'\n'+'|_|'+'\n'+'..|',5:'._.'+'\n'+'|_.'+'\n'+'._|',
+	6:'._.'+'\n'+'|_.'+'\n'+'|_|',7:'._.'+'\n'+'..|'+'\n'+'..|',8:'._.'+'\n'+'|_|'+'\n'+'|_|',
+	9:'._.'+'\n'+'|_|'+'\n'+'..|'};
+    var a=data/100;
+	var b=(data/10)%10;
+	var c=data%10;
+    conlog.(obj.a);
+    conlog.(obj.b);
+    conlog.(obj.c);
 };
+
+
